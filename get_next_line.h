7b20efbd6740da4b344d9839b954cdd35ff75ffd
Adam Olivier <adolivie@f4r11s5.clusters.42paris.fr>
2025-12-06 15:44:03 +0100

    initialisation

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adolivie <adolivie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/05 18:46:58 by adolivie          #+#    #+#             */
+/*   Updated: 2025/12/06 14:30:51 by adolivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE
+
